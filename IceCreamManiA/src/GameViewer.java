b58323c2fe173faa7be52d3647cb32d6e2284431
carolzeng <carol.zeng11@yahoo.com>
2019-04-19 21:10:17 -0500

    Created GameViewer: added print statements

---
+++ IceCreamManiA/src/GameViewer.java
@@ -0,0 +1,14 @@
+
+public class GameViewer {
+
+	public GameViewer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello");
+		System.out.print("helen is so mean to me.");
+	}
+
+}
