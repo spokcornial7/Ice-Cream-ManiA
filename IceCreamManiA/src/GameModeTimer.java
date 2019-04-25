ee7a49a839e490d448257c1fdffae2e72c34cf42
carolzeng <carol.zeng11@yahoo.com>
2019-04-25 17:58:00 -0500

    Added a countdown timer to see if it works

--- IceCreamManiA/src/GameModeTimer.java
+++ IceCreamManiA/src/GameModeTimer.java
@@ -1,9 +1,38 @@
 
+
+import java.util.Timer;
+import java.util.TimerTask;
+		
+		
 public class GameModeTimer extends GameMode
 {
 
-	public GameModeTimer() {
+	public GameModeTimer() 
+	{
 		// TODO Auto-generated constructor stub
+		 
+	}
+	
+	
+
+	//TESTING OUT THE TIMER
+	class App extends TimerTask 
+	{
+
+	    int countdown = 100;
+
+	    public void run() 
+	    {		   
+		   System.out.println(countdown);
+		   countdown--;
+		}
+
 	}
 
+	public static void main(String[] args) {
+		GameModeTimer x = new GameModeTimer();
+        Timer timer = new Timer();
+        timer.schedule(x.new App(), 0, 1000);
+    }
+	
 }
--- IceCreamManiA/src/GameViewer.java
+++ IceCreamManiA/src/GameViewer.java
@@ -5,30 +5,53 @@ import java.awt.event.*;
 
 public class GameViewer extends JFrame
 {
+	JPanel pane1;
+	GameMode game;
 	
 	public GameViewer() 
 	{
-		// TODO Auto-generated constructor stub
+		setSize(500, 500);
+		setTitle("Ice Cream Mania");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		askGameMode();
+				
 		JOptionPane.showMessageDialog(null, "");
-		JOptionPane.showMessageDialog(null, "");
+		
+		JButton restart = new JButton("Restart");
+		JButton diff = new JButton("Different Game Mode");
+		JButton exitB = new JButton("Exit Game");
 	}
-
-	public static void main(String[] args) 
+	
+	private void askGameMode()
 	{
-		JPanel pane1 = new JPanel();
+		pane1 = new JPanel();
 		JLabel welcome = new JLabel("Welcome to Ice Cream Mania!");
 		JButton mode1 = new JButton("Timer");
 		JButton mode2 = new JButton("Classic");
 		pane1.add(welcome);
 		pane1.add(mode1);
 		pane1.add(mode2);
-		
-		JOptionPane.showMessageDialog(null, "");
-		
-		JButton restart = new JButton("Restart");
-		JButton diff = new JButton("Different Game Mode");
-		JButton exitB = new JButton("Exit Game");
-	
+		add(pane1);
+		mode1.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				game = new GameModeTimer();
+			}
+		});
+		mode2.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				game = new GameModeClassic();
+			}
+		});
+	}
+
+	public static void main(String[] args) 
+	{
+		GameViewer game = new GameViewer();
+		game.setVisible(true);	
 	}
 
 }
